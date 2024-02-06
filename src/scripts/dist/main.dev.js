7bbdf5f45e1d43c6d5ff9336099ae0f9af1bb046
wilcarvalho <wil_carvalho0@icloud.com>
2024-02-06 17:46:25 -0300

    Atualização

---
+++ src/scripts/dist/main.dev.js
@@ -0,0 +1,14 @@
+"use strict";
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('form-sorteador').addEventListener('submit', function (evento) {
+    evento.preventDefault();
+    var numeroMaximo = document.getElementById('numero-maximo').value;
+    numeroMaximo = parseInt(numeroMaximo);
+    var numeroAleatorio = Math.random() * numeroMaximo;
+    numeroAleatorio = Math.floor(numeroAleatorio + 1);
+    document.getElementById('resultado-valor').innerText = numeroAleatorio;
+    document.querySelector('.resultado').style.display = 'block';
+  });
+});
+//# sourceMappingURL=main.dev.js.map
